439cab3837980a6c3832e15a90e89e09d28466b1
Adam Gillfillan <agillfi@ncsu.edu>
2018-03-26 23:43:30 -0400

    Initial commit of craftlabs

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import ComingSoon from '../components/ComingSoon';
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'ComingSoon',
+      component: ComingSoon,
+    },
+  ],
+});
